4fc9939859c338df1344642a0d48a56cacd6c573
admin <lazar.viktor@gmail.com>
2021-07-31 12:38:27 +0200

    enforcing 2 file upload

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var multer = require("multer");
+const maxSize = 1 * 1024 * 1024; // for 1MB
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/images");
@@ -20,16 +21,32 @@ var storage = multer.diskStorage({
     }
     cb(null, "image-" + Date.now() + "." + filetype);
   },
+  limits: { fileSize: maxSize },
 });
-var upload = multer({ storage: storage });
+var upload = multer({ storage: storage }).array("files", 2);
 
-router.post("/upload", upload.single("file"), function (req, res, next) {
-  console.log(req.file);
-  if (!req.file) {
-    res.status(500);
-    return next(err);
-  }
-  res.json({ fileUrl: "http://localhost:3000/images/" + req.file.filename });
+router.post("/upload3", (req, res) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.status(400);
+      res.send(err);
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.status(500);
+      res.send(err);
+    }
+
+    // Everything went fine.
+    console.log(req.files);
+    var responseFileNamesArray = [];
+    req.files.forEach(function (item, index) {
+      responseFileNamesArray.push(
+        "http://localhost:3000/images/" + item.filename
+      );
+    });
+    res.json({ fileUrls: responseFileNamesArray });
+  });
 });
 
 /* GET home page. */
