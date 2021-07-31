6c3f4100721204145ae6a783255d7402c243348b
admin <lazar.viktor@gmail.com>
2021-07-31 16:59:46 +0200

    added input checks, constants, and errorCodes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var router = express.Router();
+let constants = require("../utils/const.js").constants;
+let errorCodes = require("../utils/errorCodes.js").errorCodes;
 
 var multer = require("multer");
-const maxSize = 1 * 1024 * 1024; // for 1MB
+let fileFilter = function (req, file, cb) {
+  var allowedMimes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedMimes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      {
+        success: false,
+        message: "Invalid file type. Only jpg, png image files are allowed.",
+      },
+      false
+    );
+  }
+};
+
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/images");
@@ -19,33 +35,61 @@ var storage = multer.diskStorage({
     if (file.mimetype === "image/jpeg") {
       filetype = "jpg";
     }
+    if (file.mimetype === "image/jpg") {
+      filetype = "jpg";
+    }
     cb(null, "image-" + Date.now() + "." + filetype);
   },
-  limits: { fileSize: maxSize },
+  fileFilter: fileFilter,
 });
-var upload = multer({ storage: storage }).array("files", 2);
 
-router.post("/upload3", (req, res) => {
+var upload = multer({
+  storage: storage,
+  limits: { fileSize: constants.MAXFILESIZE },
+}).array("files", constants.NUMBER_OF_ACCEPTED_FILES);
+
+router.post("/upload", (req, res) => {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading.
       res.status(400);
-      res.send(err);
+      if (err.code == errorCodes.LIMIT_UNEXPECTED_FILE) {
+        res.send({
+          errorCode: errorCodes.LIMIT_UNEXPECTED_FILE_ERROR_CODE,
+          error: errorCodes.LIMIT_UNEXPECTED_FILE_ERROR_MESSAGE,
+        });
+      } else if (err.code == errorCodes.LIMIT_FILE_SIZE) {
+        res.send({
+          errorCode: errorCodes.LIMIT_FILE_SIZE_ERROR_CODE,
+          error: errorCodes.LIMIT_FILE_SIZE_ERROR_MESSAGE,
+        });
+      } else {
+        res.send(err);
+      }
     } else if (err) {
       // An unknown error occurred when uploading.
       res.status(500);
       res.send(err);
+    } else {
+      // check if we received constants.NUMBER_OF_ACCEPTED_FILES files
+      if (req.files.length != constants.NUMBER_OF_ACCEPTED_FILES) {
+        res.status(400);
+        res.json({
+          errorCode: errorCodes.INCORRECT_AMOUNT_OF_FILES_ERROR_CODE,
+          error: errorCodes.INCORRECT_AMOUNT_OF_FILES_ERROR_MESSAGE,
+        });
+      } else {
+        // Everything went fine.
+        console.log(req.files);
+        var responseFileNamesArray = [];
+        req.files.forEach(function (item, index) {
+          responseFileNamesArray.push(
+            "http://localhost:3000/images/" + item.filename
+          );
+        });
+        res.json({ fileUrls: responseFileNamesArray });
+      }
     }
-
-    // Everything went fine.
-    console.log(req.files);
-    var responseFileNamesArray = [];
-    req.files.forEach(function (item, index) {
-      responseFileNamesArray.push(
-        "http://localhost:3000/images/" + item.filename
-      );
-    });
-    res.json({ fileUrls: responseFileNamesArray });
   });
 });
 
